6cadea251f5f5ced95c492c19ae5179aeab2c32c
Ekaterina-spec-6553 <ekaterinarunova6553@gmail.com>
2024-07-31 10:55:50 +0300

    module_2_5.py

--- module_2_5.py
+++ module_2_5.py
@@ -4,6 +4,11 @@ def get_matrix(n, m, value):
         matrix.append([])
         for j in range(m):
             matrix[i].append(value)
-    print(matrix)
+    return matrix
 
-get_matrix(4, 3, 12)
+result1 = get_matrix(3, 4, 12)
+result2 = get_matrix(2, 5, 10)
+result3 = get_matrix(4, 2, 8)
+print(result1)
+print(result2)
+print(result3)
